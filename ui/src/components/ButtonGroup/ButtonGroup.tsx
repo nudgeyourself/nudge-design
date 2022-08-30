2fa8d2f4226106324738524580a590781650a15f
Russ Campbell <russ@russcampbell.net>
2022-08-30 09:32:00 -0500

    Added new margin prop to ButtonGroup

--- ui/src/components/ButtonGroup/ButtonGroup.tsx
+++ ui/src/components/ButtonGroup/ButtonGroup.tsx
@@ -6,11 +6,16 @@ import styles from "./ButtonGroup.module.css";
 export interface ButtonGroupProps {
   children: React.ReactNode;
   row?: boolean;
+  margin?: boolean;
 }
 
 const ButtonGroup = (props: ButtonGroupProps) => {
   return (
-    <div className={`${styles.buttonGroup} ${props.row ? styles.row : ''}`}>
+    <div
+      className={`${styles.buttonGroup} ${props.row ? styles.row : ""} ${
+        props.margin ? styles.margin : ""
+      }`}
+    >
       {props.children}
     </div>
   );

21d8d1220828585ab7c53f86dd02fad1cac91507
Russ Campbell <russ@russcampbell.net>
2022-10-28 13:07:43 -0500

    Docs test

--- docs/build/assets/js/fadbd120.cf3107fb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[605],{7099:e=>{e.exports=JSON.parse('{"label":"hello","permalink":"/nudge-design/docs/blog/tags/hello","allTagsPath":"/nudge-design/docs/blog/tags","count":2}')}}]);
--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "Nudge Design Library",
   tagline: "Welcome to the Nudge Design library.",
   url: "https://nudgeyourself.github.io/",
-  baseUrl: "/nudge-design/docs/",
+  baseUrl: "/nudge-design/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",

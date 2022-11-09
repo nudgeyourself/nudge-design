1d8e6c88f9eefff67f646e0dba3cb7232f339555
Russ Campbell <russ@russcampbell.net>
2022-11-09 15:49:02 -0600

    Added tokens sections

--- docs/docs/tokens.md
+++ docs/docs/tokens.md
@@ -10,15 +10,25 @@ import TokenTable from '@site/src/components/TokenTable';
 ## Prefixes
 
 ### Component
-Design decisions used across multiple components. When developing and looking for applicable tokens always start here. 
+
+Design decisions used across multiple components. When developing and looking for applicable tokens always start here.
+
+<TokenTable category="component" />
 
 ### Semantic
+
 Design decisions attached to a particular meaning or use case.
 
+<TokenTable category="semantic" />
+
 ### Core
+
 Raw values that are used by the system. Not all core tokens may be in use. These tokens represent all potential values that are compliant with the system. Avoid referencing core tokens directly whenever possible.
 
+<TokenTable category="core" />
+
 ### Theme
+
 Tokens that can be customized for white-labels.
 
-<TokenTable />
+<TokenTable category="theme" />
--- docs/src/components/TokenTable.js
+++ docs/src/components/TokenTable.js
@@ -13,13 +13,46 @@ const ColorSwatch = (obj) => {
         borderRadius: "var(--core-size-4)",
         backgroundColor: obj.value,
         display: "inline-block",
+        border: "1px solid var(--semantic-border-default)",
       }}
     ></div>
   );
 };
 
-const TokenTable = () => {
+const FontExample = (props) => {
+  const { obj } = props;
+  return (
+    <div
+      style={{
+        fontSize:
+          obj.type === "fontSizes" ? obj.value : "var(--core-font-size-2)",
+        fontWeight:
+          obj.type === "fontWeights" ? obj.value : "var(--core-font-weight-2)",
+      }}
+    >
+      Aa
+    </div>
+  );
+};
+
+const SizeBox = (props) => {
+  const { size } = props;
+  return (
+    <div
+      style={{
+        display: "inline-block",
+        width: size,
+        height: size,
+        border: "1px solid var(--core-color-black)",
+        borderStyle: "dashed",
+      }}
+    ></div>
+  );
+};
+
+const TokenTable = (props) => {
   const [parsedTokens, setParsedTokens] = useState([]);
+  const { category } = props;
 
   function parseTokens(obj, arr) {
     for (var k in obj) {
@@ -39,28 +72,43 @@ const TokenTable = () => {
 
   useEffect(() => {
     var pt = [];
-    parseTokens(tokens, pt);
+    if (category) {
+      parseTokens(tokens[category], pt);
+    } else {
+      parseTokens(tokens, pt);
+    }
+
     setParsedTokens(pt);
   }, []);
 
   return (
     <table>
-      <tr>
-        <td>Name</td>
-        <td>Value</td>
-        <td>Demo</td>
-      </tr>
-      {parsedTokens.map((obj) => {
-        return (
-          <tr>
-            <td>{obj.name}</td>
-            <td>{obj.value}</td>
-            <td>
-              {obj.type === "color" ? <ColorSwatch value={obj.value} /> : "-"}
-            </td>
-          </tr>
-        );
-      })}
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Value</td>
+          <td>Demo</td>
+        </tr>
+        {parsedTokens.map((obj, i) => {
+          return (
+            <tr key={i.toString()}>
+              <td>{obj.name}</td>
+              <td>{obj.value}</td>
+              <td>
+                {obj.type === "color" ? (
+                  <ColorSwatch value={obj.value} />
+                ) : obj.type === "sizing" || obj.type === "spacing" ? (
+                  <SizeBox size={obj.value} />
+                ) : obj.type === "fontWeights" || obj.type === "fontSizes" ? (
+                  <FontExample obj={obj} />
+                ) : (
+                  "-"
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 };
